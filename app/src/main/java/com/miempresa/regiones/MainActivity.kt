493efcbe8a48a6706caf670e6e73c7db86136c40
rocio peralta <deliarocio08@gmail.com>
2022-05-13 01:00:20 -0500

    Interfaces

---
+++ app/src/main/java/com/miempresa/regiones/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.miempresa.regiones
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+import com.miempresa.regiones.ui.theme.RegionesTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalComposeUiApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            app()
+
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun app() {
+    var expanded by remember { mutableStateOf(false) }
+    val suggestions = listOf("Costa", "Sierra", "Selva")
+    var selectedText by remember { mutableStateOf("") }
+
+    var textfieldSize by remember { mutableStateOf(Size.Zero) }
+
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .height(400.dp)
+            .background(Color.White)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.region),
+            contentDescription = "logo",
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+
+        )
+
+
+        Text(
+            text = "BIENVENIDO", fontSize = 32.sp,
+            color = Color.Black,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Elija una región:", fontSize = 20.sp,
+            color = Color.Blue,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+
+        )
+
+
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    //This value is used to assign to the DropDown the same width
+                    textfieldSize = coordinates.size.toSize()
+                },
+            label = { Text("Regiones") },
+            trailingIcon = {
+                Icon(icon, "contentDescription",
+                    Modifier.clickable { expanded = !expanded })
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textfieldSize.width.toDp() })
+        ) {
+            suggestions.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    selectedText = label
+                    expanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
+
+    }
+
+}
+
